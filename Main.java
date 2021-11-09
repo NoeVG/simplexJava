96f1d7472dff1e71936034deb199cdae95fabed6
Noe VG <noe-vg@outlook.com>
2021-11-09 13:42:33 -0600

    update repo

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * This class it's initial to Simplex
+ */
+public Main{
+    
+    /**
+     * @author: NoeVG
+     * @about: shalalalala
+     */
+    public static void main(String[] args) {
+    
+    }
+}
