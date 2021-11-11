695058c53853e047c17c965fa6bea0c8e8fe1443
Ziusudra <ziusudra@ziusudra-workstation.huawei.net>
2021-11-11 13:16:32 -0600

    add: code by Luis

---
+++ Simplex/src/main/java/noevg/github/io/simplex/algebraicExpression/Term.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package noevg.github.io.simplex.algebraicExpression;
+
+/**
+ *
+ * @author ziusudra
+ */
+public class Term {
+    int coefficient;
+    char variable;
+    int exponent;
+    
+    public void setCoefficient(int coefficient){
+        this.coefficient = coefficient;
+    }
+}
